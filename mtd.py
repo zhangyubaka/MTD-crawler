11caf7385624877b897181c2322b3de7bb6d3f38
zhangyubaka <zhangyubaka@gmail.com>
2017-12-13 13:52:17 +0800

    Add eventloop and fix logic

--- mtd.py
+++ mtd.py
@@ -4,15 +4,16 @@
 import aiohttp
 from lxml import etree, html
 from pprint import pprint
+import asyncio
 
 
-async def getPage(url: str):
+async def get_page(url: str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url) as resp:
             return await resp.text()
 
 
-async def extractLinks(page, xpath: str) -> list:
+async def extract_links(page, xpath: str) -> list:
     root = etree.fromstring(page, etree.HTMLParser())
     links = root.xpath(xpath)
     l = []
@@ -21,9 +22,13 @@ async def extractLinks(page, xpath: str) -> list:
     return l
 
 
-if __name__ == '__main__':
+async def main():
     url = input('Please enter the url for scrapping: ')
-    subpages = extractLinks(getPage(url), xpath='//dd/h3/a[contain(href)]')
-    links
+    subpages = await extract_links(await get_page(url), xpath='//dd/h3/a[contain(href)]')
     for i in subpages:
-        pprint(extractLinks(getPage(i), xpath='//ul[@id="dl-btn"]//li//a'))
+        pprint(await extract_links(await get_page(i), xpath='//ul[@id="dl-btn"]//li//a'))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
