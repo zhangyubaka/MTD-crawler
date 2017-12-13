88913ec456be4f605786b73a4e83fc0cd5237869
zhangyubaka <zhangyubaka@gmail.com>
2017-12-13 14:11:07 +0800

    Using coloredlogs

--- mtd.py
+++ mtd.py
@@ -5,19 +5,28 @@ import aiohttp
 from lxml import etree, html
 from pprint import pprint
 import asyncio
+import logging
+import coloredlogs
+
+logger = logging.getLogger()
+coloredlogs.install(logger=logger)
 
 
 async def get_page(url: str) -> str:
+    logger.info('Getting page...')
     async with aiohttp.ClientSession() as session:
         async with session.get(url=url) as resp:
+            logger.debug('Getting '+url)
             return await resp.text()
 
 
 async def extract_links(page, xpath: str) -> list:
+    logger.info("Extracting links...")
     root = etree.fromstring(page, etree.HTMLParser())
     links = root.xpath(xpath)
     l = []
     for i in links:
+        logger.debug("Extracting "+i)
         l.append(i.items()[0][1])
     return l
 
